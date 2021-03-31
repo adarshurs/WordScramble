7a6ef7b411eba0b7e159518d13e32ef60839f9fc
Adarsh Urs <adarshrajurs@gmail.com>
2021-03-31 15:30:06 +0530

    Added all files and folders

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Adarsh Urs on 20/03/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
